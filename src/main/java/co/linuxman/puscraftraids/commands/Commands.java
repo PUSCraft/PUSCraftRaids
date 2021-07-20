d4efb87c0dc66b1282c7fab4bd2ad1e9cc3212b2
xlinuxmanx <linuxman@linuxman.co>
2021-07-20 10:09:32 -0500

    Added Cancel Command to new Commands class. Set it to check for the command then arguments given.

--- src/main/java/co/linuxman/puscraftraids/commands/Commands.java
+++ src/main/java/co/linuxman/puscraftraids/commands/Commands.java
@@ -1,32 +1,39 @@
 package co.linuxman.puscraftraids.commands;
 
 import co.linuxman.puscraftraids.PUSCraftRaids;
+import co.linuxman.puscraftraids.raids.RaidManager;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 public class Commands implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command cmd, @NotNull String label, @NotNull String[] args) {
-        Player player = ((Player)sender);
+        RaidManager raidManager = new RaidManager();
 
         if(sender.hasPermission("puscraftraids.admin")){
-            if(args.length == 0){
-                //Make Fancy Help
-                player.sendMessage("[RaidsPerRegion] Invalid arguments");
-                player.sendMessage("[RaidsPerRegion] Reload Config: /raid reload");
-                player.sendMessage("[RaidsPerRegion] View Source Code: /raid source");
-            }else if(cmd.getName().equalsIgnoreCase("source")){
-                //Message with source link
-                String website = String.format("Source: %s",PUSCraftRaids.getPlugin().getDescription().getWebsite());
-                player.sendMessage(website);
-            }else if(cmd.getName().equalsIgnoreCase("reload")){
-                //Reload Config
-                PUSCraftRaids.getPlugin().reloadConfig();
-                player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&aReloaded Config"));
+            if(cmd.getName().equalsIgnoreCase("raids") || cmd.getName().equalsIgnoreCase("puscraftraids")){
+                if(args.length == 0){
+                    //Make Fancy Help
+                    sender.sendMessage("[RaidsPerRegion] Invalid arguments");
+                    sender.sendMessage("[RaidsPerRegion] Reload Config: /raid reload");
+                    sender.sendMessage("[RaidsPerRegion] View Source Code: /raid source");
+                }else if(args[0].equalsIgnoreCase("source")){
+                    //Message with source link
+                    String website = String.format("Source: %s",PUSCraftRaids.getPlugin().getDescription().getWebsite());
+                    sender.sendMessage(website);
+                }else if(args[0].equalsIgnoreCase("reload")){
+                    //Reload Config
+                    PUSCraftRaids.getPlugin().reloadConfig();
+                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&aReloaded Config"));
+                }else if (args.length == 1 && args[0].equalsIgnoreCase("cancel")){
+                    if(raidManager.isRaidInProgress){
+                        raidManager.cancelRaid();
+                        sender.sendMessage("[RaidsPerRegion] Raid has been canceled");
+                    }
+                }
             }
         }
         sender.sendMessage("You do not have permission for this command");
